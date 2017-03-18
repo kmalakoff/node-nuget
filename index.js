09f1c9d7fc4a7dbc67f496713b1cfe7e56729579
Kevin Malakoff <kmalakoff@gmail.com>
2017-03-18 14:13:22 -0700

    Bump to 2.0.1

--- index.js
+++ index.js
@@ -14,27 +14,23 @@ const NUGET_EXE = path.resolve(path.join(__dirname, './bin/NuGet.exe'));
 const runCommand = function(command, arg_or_args) {
   let args = [NUGET_EXE, command];
   args = args.concat((_.isArray(arg_or_args)) ? arg_or_args : [arg_or_args]);
-  if (process.platform !== 'win32') { args.unshift('mono'); }
+  if (process.platform !== 'win32') args.unshift('mono');
   return exec(args.join(' '));
 };
 
-const debounceCallback = function(callback) {
-  var debounced_callback = function() { if (debounced_callback.was_called) { return; debounced_callback.was_called = true; return callback.apply(null, Array.prototype.slice.call(arguments, 0)); } };
-  return debounced_callback;
-};
-
 const getFile = function(file, callback) {
-  if (file.pipe) { return callback(null, file); }
-  return vinyl.src(file)
+  if (file.pipe) return callback(null, file);
+
+  vinyl.src(file)
     .pipe(es.writeArray(function(err, files) {
-      if (err) { return callback(err); }
+      if (err) return callback(err);
       if ((files.length === 0) || (files.length > 1)) { return callback(new Error(`Expecting one file for ${file}. Found ${files.length}`)); }
       return callback(null, files[0]);
     })
   );
 };
 
-const randomFilename = () => crypto.createHash('sha1').update(new Date().getTime().toString()+_.uniqueId()).digest('hex');
+const randomFilename = () => `${crypto.createHash('sha1').update(new Date().getTime().toString()+_.uniqueId()).digest('hex')}.nupkg`;
 
 module.exports = NodeNuget = class NodeNuget {
   static setApiKey(key, callback) {
@@ -43,41 +39,26 @@ module.exports = NodeNuget = class NodeNuget {
   }
 
   static pack(file, callback) {
-    return getFile(file, function(err, file) {
-      if (err) { return callback(err); }
+    getFile(file, function(err, file) {
+      if (err) return callback(err);
 
-      return file.pipe(es.wait(function(err, data) {
-        let item, missing_files;
-        if (err) { return callback(err); }
+      file.pipe(es.wait(function(err, data) {
+        if (err) return callback(err);
 
         const package_desc = et.parse(data.toString());
         const package_id = package_desc.findtext('./metadata/id');
         const package_version = package_desc.findtext('./metadata/version');
 
-        const files = ((() => {
-          const result = [];
-          for (item of Array.from(package_desc.findall('./files/file'))) {
-            result.push(path.join(path.dirname(file.path), item.attrib != null ? item.attrib.src : undefined));
-          }
-          return result;
-        })());
-        if ((missing_files = ((() => {
-          const result1 = [];
-          for (item of Array.from(file)) {             if (!fs.existsSync(item)) {
-              result1.push(item);
-            }
-          }
-          return result1;
-        })())).length) {
-          return callback(new Error(`Nuget: cannot build ${file.path}. Missing files: ${missing_files}`));
-        }
+        const files = package_desc.findall('./files/file').map(x => path.join(path.dirname(file.path), x.attrib ? x.attrib.src : undefined));
+        const missing_files = files.filter(x => !fs.existsSync(x));
+        if (missing_files.length) return callback(new Error(`Nuget: cannot build ${file.path}. Missing files: ${missing_files}`));
 
         if (runCommand('pack', file.path).code !== 0) { return callback(new Error(`Failed to pack file: ${file.path}`)); }
 
         const package_path = path.resolve(path.join(process.cwd(), '.', `${package_id}.${package_version}.nupkg`));
-        return getFile(package_path, function(err, file) {
-          if (err) { return callback(err); }
-          return fs.unlink(package_path, () => callback(err, file));
+        getFile(package_path, function(err, file) {
+          if (err) return callback(err);
+          fs.unlink(package_path, () => callback(err, file));
         });
       })
       );
@@ -85,19 +66,19 @@ module.exports = NodeNuget = class NodeNuget {
   }
 
   static push(file, callback) {
-    let file_path = null; let owned = false;
+    let file_path = null;
+    let owned = false;
 
     const queue = new Queue(1);
 
     // ensure there is a file on disk
     queue.defer(function(callback) {
-      if (!file.pipe) { return callback(null, (file_path = file)); }
-      if (fs.existsSync(file_path = file.path)) { return callback(); } // use if exists on disk
+      if (!file.pipe) return callback(null, (file_path = file));
+      if (fs.existsSync(file_path = file.path)) return callback(); // use if exists on disk
 
-      callback = debounceCallback(callback);
+      callback = _.once(callback);
       file_path = randomFilename(); owned = true;
-      return file
-        .pipe(fs.createWriteStream(file_path))
+      file.pipe(fs.createWriteStream(file_path))
         .on('finish', callback)
         .on('error', callback);
     });
